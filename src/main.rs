1a5d1dbf7ad6329475271841c16ba8554072a17b
Alexandre Zanin <zanin.alexandre.pg@gmail.com>
2020-01-31 09:23:28 +0100

    Basic hierarchy for MVC

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod data;
+
+mod controller;
+mod view;
+
 fn main() {
-    println!("Hello, world!");
+    // For now, main will contain the Model...
+    controller::do_stuff();
+    view::present();
 }
---
+++ src/view/mod.rs
@@ -0,0 +1,3 @@
+pub fn present() {
+    print!("present");
+}
